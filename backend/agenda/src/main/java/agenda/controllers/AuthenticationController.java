be6685682e073936b823eb0f3956ca6f598bd960
Yuri Neves <yurineves1934@gmail.com>
2023-10-21 00:45:31 -0300

    feat: create controller Authenticate

---
+++ backend/agenda/src/main/java/agenda/controllers/AuthenticationController.java
@@ -0,0 +1,43 @@
+package agenda.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Controller;
+
+import agenda.config.TokenService;
+import agenda.entities.User;
+import agenda.services.UserService;
+
+@Controller
+public class AuthenticationController {
+
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
+  @Autowired
+  private UserService userService;
+
+  @Autowired
+  private TokenService tokenService;
+
+  @MutationMapping
+  public String login(@Argument String login, @Argument String password) {
+    var usernamepassword = new UsernamePasswordAuthenticationToken(login, password);
+
+    var auth = authenticationManager.authenticate(usernamepassword);
+
+    var token = tokenService.generateToken((User) auth.getPrincipal());
+
+    return token;
+  }
+
+  @MutationMapping
+  public User register(@Argument String login, @Argument String password, @Argument String name, @Argument String cpf,
+      @Argument String phone, @Argument String birth, @Argument String role) {
+
+    return userService.createUser(login, password, name, cpf, phone, birth, role);
+  }
+}
