64fa199f1529ab6a3b77e986f63423d05ae6ce74
Yuri Neves <yurineves1934@gmail.com>
2023-10-22 13:51:39 -0300

    feat: create context authenticate

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useState, ReactNode, useEffect } from 'react';
+
+interface AuthContextProps {
+  isAuthenticated: boolean;
+  login: (token: string) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    if(localStorage.getItem("accessToken")) {setIsAuthenticated(true);}
+  },[])
+
+  const login = () => {
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth deve ser usado dentro de um AuthProvider');
+  }
+  return context;
+};
