e687921a553f6ce5bee9b19f6140b531c4900e57
Yuri Neves <yurineves1934@gmail.com>
2023-10-22 13:52:15 -0300

    feat: create mask input forms

---
+++ frontend/src/masks/masks.ts
@@ -0,0 +1,45 @@
+export const normalizePhoneNumber = (value: string | undefined) => {
+  if (!value) return '';
+
+  return value
+    .replace(/[\D]/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{5})(\d)/, '$1-$2')
+    .replace(/(-\d{4})(\d+?)/, '$1');
+};
+
+export const normalizeCnpjNumber = (value: string | undefined) => {
+  if (!value) return '';
+
+  return value
+    .replace(/[\D]/g, '')
+    .replace(/(\d{2})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1/$2')
+    .replace(/(\d{4})(\d)/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1');
+};
+
+export const normalizeCepNumber = (value: string | undefined) => {
+  if (!value) return '';
+  return value
+    .replace(/\D/g, '')
+    .replace(/^(\d{5})(\d{3})+?$/, '$1-$2')
+    .replace(/(-\d{3})(\d+?)/, '$1');
+};
+
+export const normalizeCpfNumber = (value: string | undefined) => {
+  if (!value) return '';
+
+  return value
+    .replace(/[\D]/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1');
+};
+
+export const normalizeEmail = (value: string | undefined) => {
+  if (!value) return '';
+  return value.trim();
+};
